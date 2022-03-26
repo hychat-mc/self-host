509ff9b0f007eef2243d5d2f008cf54fcbe563b3
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-03-27 09:54:08 +1100

    feat: send message when  bot is ready

--- src/events/mineflayer/Login.ts
+++ src/events/mineflayer/Login.ts
@@ -4,6 +4,8 @@ export const event: Event = {
 	name: 'login',
 	runOnce: true,
 	run: async (bot) => {
+		await bot.sendToDiscord('gc', ' The bot has logged in and is now ready!');
+
 		setInterval(() => {
 			bot.executeCommand('/g online');
 		}, 60_000 * 5);
