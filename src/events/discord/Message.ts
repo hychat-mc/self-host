83e03d3b555424bca5f7a47a453dbeae23710ee3
John Doe <69335633+Jejebecarte@users.noreply.github.com>
2022-03-26 12:59:12 +1100

    feat: censor profane language

--- src/events/discord/Message.ts
+++ src/events/discord/Message.ts
@@ -1,6 +1,7 @@
 import { Event } from '../../interfaces/Event';
 import { Message, Util } from 'discord.js';
 import Bot from '../../classes/Bot';
+import BadWords from '../../util/BadWords';
 
 export const event: Event = {
 	name: 'messageCreate',
@@ -20,13 +21,18 @@ export const event: Event = {
 			return;
 		}
 
-		message.content = `${message.member.displayName} > ${Util.escapeMarkdown(message.content)}`;
-
-		await bot.sendGuildMessage(message.channel === bot.memberChannel ? 'gc' : 'oc', message.content);
 		try {
 			await message.delete();
 		} catch {
-			await message.channel.send(`warning: ${message.author.username}, could not delete message.`);
+			await message.channel.send(`Warning: ${message.author.username}, could not delete message.`);
 		}
+
+		if (BadWords.some((word) => message.content.includes(word))) {
+			await message.channel.send(`Warning: ${message.author.username}, you may not use profane language!`);
+			return;
+		}
+
+		message.content = `${message.member.displayName} > ${Util.escapeMarkdown(message.content)}`;
+		await bot.sendGuildMessage(message.channel === bot.memberChannel ? 'gc' : 'oc', message.content);
 	},
 };
