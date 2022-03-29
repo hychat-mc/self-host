0027b90235dbe3d51dd2dc74d94695af64057d8b
Matt <46137770+xMdb@users.noreply.github.com>
2022-03-29 22:32:38 +1000

    feat: add changeable guild rank via env

--- src/events/mineflayer/GuildChat.ts
+++ src/events/mineflayer/GuildChat.ts
@@ -12,8 +12,10 @@ export default {
 		const channel = messageArray[0] as 'Guild' | 'Officer';
 		const hypixelRank = messageArray[1] as string | null;
 		const playerName = messageArray[2] as string;
-		const guildRank = messageArray[3] as string | null;
+		let guildRank = messageArray[3] as string | null;
 		const chatMessage = messageArray[4] as string;
+
+		if (process.env.DISPLAY_GUILD_RANK === 'false') guildRank = null;
 
 		const formattedMessage = ` **${hypixelRank ?? ''}${Util.escapeMarkdown(playerName)}${
 			' ' + guildRank ?? ''
